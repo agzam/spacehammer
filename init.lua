fa40d54a90bbe7e4a2491a919fef625b3ea44b64
Ag Ibragimov <agzam.ibragimov@gmail.com>
2016-07-11 00:09:41 -0700

    Added readme

--- init.lua
+++ init.lua
@@ -1,6 +1,34 @@
 require "preload"
 
--- bind application hotkeys
+-- Modal activation / deactivation
+modal = hs.hotkey.modal.new({"ctrl", "cmd"}, "space")
+mainModalText = "w - windows\na - apps"
+function modal:entered() alert(mainModalText, 999999 ) end
+modal:bind("","escape", function() modal:exit() end)
+function modal:exited() hs.alert.closeAll() end
+
+modalW = hs.hotkey.modal.new();
+modal:bind("","w", function() modalW:enter() end)
+
+winmanText = "hjkl \t\t\t\t jumping\ncmd + hjkl \t halves\nalt + hjkl \t\t increments\nshift + hjkl \t resize\ng \t\t\t\t\t grid\nm \t\t\t\t maximize\nu \t\t\t\t\t undo"
+
+function modalW:entered() hs.alert.closeAll(); alert(winmanText, 999999) end
+
+modalA = hs.hotkey.modal.new();
+modal:bind("","a", function() modalA:enter() end)
+
+appsModalText = "e \t emacs\nb \t chrome\nt \t terminal\ns \t slack"
+function modalA:entered() hs.alert.closeAll(); alert(appsModalText, 999999) end
+
+modalW:bind("","escape", function() exitModals() end)
+
+function exitModals()
+  modalA:exit(); modalW:exit(); modal:exit()
+end
+
+require "window"
+
+-- applications modal
 hs.fnutils.each({
     { key = "t", app = "iTerm" },
     { key = "b", app = "Google Chrome" },
@@ -18,33 +46,8 @@ hs.fnutils.each({
       end
     end
 
-    hs.hotkey.bind(hyper, item.key, appActivation)
+    modalA:bind("", item.key, function() appActivation(); exitModals() end)
 end)
-
--- Modal activation / deactivation
-modal = hs.hotkey.modal.new({"ctrl", "cmd"}, "space")
-mainModalText = "w - windows"
-function modal:entered() alert(mainModalText, 999999 ) end
-modal:bind("","escape", function() modal:exit() end)
-function modal:exited() hs.alert.closeAll() end
-
-modalW = hs.hotkey.modal.new();
-modal:bind("","w", function() modalW:enter() end)
-
-winmanText = "hjkl \t\t\t\t jumping\ncmd + hjkl \t halves\nalt + hjkl \t\t increments\nshift + hjkl \t resize\ng \t\t\t\t\t grid\nm \t\t\t\t maximize"
-
-function modalW:entered()
-  hs.alert.closeAll()
-  alert(winmanText, 999999)
-end
-
-modalW:bind("","escape", function() exitModals() end)
-
-function exitModals()
-  modalW:exit(); modal:exit()
-end
-
-require "window"
 
 hs.alert.show("Config Loaded")
 
