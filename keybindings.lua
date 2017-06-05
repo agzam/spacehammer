1c1ad457dd09393922ae23aa4b4f373078d6f071
Ag Ibragimov <agzam.ibragimov@gmail.com>
2017-06-05 00:43:50 -0700

    Moves conflicting native Chrome binding
    Cmd+L - jump to address bar in Chrome now moved to Cmd+Shift+L

--- keybindings.lua
+++ keybindings.lua
@@ -1,48 +1,5 @@
 local utils = require "utils"
 
-local REPEAT_FASTER = 10 * 1000
-local REPEAT_SLOWER = 100 * 1000
-local NO_REPEAT = -1
-
-local function keyStroke(mod, key, repeatDelay)
-    hs.eventtap.event.newKeyEvent(mod, key, true):post()
-    if repeatDelay <= 0 then
-        repeatDelay = REPEAT_FASTER
-    end
-    hs.timer.usleep(repeatDelay)
-    hs.eventtap.event.newKeyEvent(mod, key, false):post()
-end
-
-local function keyStrokeSystem(key, repeatDelay)
-    hs.eventtap.event.newSystemKeyEvent(key, true):post()
-    if repeatDelay <= 0 then
-        repeatDelay = REPEAT_FASTER
-    end
-    hs.timer.usleep(repeatDelay)
-    hs.eventtap.event.newSystemKeyEvent(key, false):post()
-end
-
--- Map sourceKey + sourceMod -> targetKey + targetMod
-local function keymap(sourceKey, sourceMod, targetKey, targetMod, repeatDelay)
-    sourceMod = sourceMod or {}
-
-    repeatDelay = repeatDelay or REPEAT_FASTER
-    noRepeat = repeatDelay <= 0
-
-    local fn = nil
-    if targetMod == nil then
-        fn = hs.fnutils.partial(keyStrokeSystem, string.upper(targetKey), repeatDelay)
-    else
-        targetMod = utils.splitStr(targetMod, '+')
-        fn = hs.fnutils.partial(keyStroke, targetMod, targetKey, repeatDelay)
-    end
-    if noRepeat then
-        hs.hotkey.bind(sourceMod, sourceKey, fn, nil, nil)
-    else
-        hs.hotkey.bind(sourceMod, sourceKey, fn, nil, fn)
-    end
-end
-
 -- ------------------
 -- simple vi-mode 
 -- ------------------
@@ -54,9 +11,9 @@ local arrows = {
 }
 local enableSimpleViMode = function()
   for k, v in pairs(arrows) do
-    keymap(k, 'alt', v, '')
-    keymap(k, 'alt+shift', v, 'alt')
-    keymap(k, 'alt+shift+ctrl', v, 'shift')
+    utils.keymap(k, 'alt', v, '')
+    utils.keymap(k, 'alt+shift', v, 'alt')
+    utils.keymap(k, 'alt+shift+ctrl', v, 'shift')
   end
 end
 local disableSimpleViMode = function()
@@ -73,7 +30,7 @@ utils.applyAppSpecific({'Emacs'}, enableSimpleViMode, nil, true)
 local left_right = {h = '[', l = ']'}
 local enableSimpleTabSwithing = function()
   for k, v in pairs(left_right) do
-    keymap(k, 'cmd', v, 'cmd+shift')
+    utils.keymap(k, 'cmd', v, 'cmd+shift')
   end
 end
 local disableSimpleTabSwitching = function()
@@ -86,6 +43,15 @@ local tabSwitchIn = {'Google Chrome', 'iTerm2'}
 utils.applyAppSpecific(tabSwitchIn, enableSimpleTabSwithing, nil, nil)
 utils.applyAppSpecific(tabSwitchIn, disableSimpleTabSwitching, nil, true)
 
+--- setting conflicting Cmd+L (jump to address bar) keybinding to Cmd+Shift+L
+utils.applyAppSpecific({'Google Chrome'},
+  function()
+    hs.hotkey.bind({'cmd', 'shift'}, 'l', function()
+        local app = hs.window.focusedWindow():application()
+        app:selectMenuItem({'File', 'Open Location…'})
+    end)
+  end, nil, nil)
+
 -- ----------------------------
 -- App switcher with Cmd++j/k 
 -- ----------------------------
@@ -96,5 +62,11 @@ switcher = hs.window.switcher.new(utils.globalfilter, {textSize = 12,
                                                        selectedThumbnailSize = 640,
                                                        backgroundColor = {0, 0, 0, 0}})
 
-hs.hotkey.bind('cmd','j', function() switcher:next() end)
-hs.hotkey.bind('cmd','k', function() switcher:previous() end)
+hs.hotkey.bind({'cmd'},'j', function() switcher:next() end)
+hs.hotkey.bind({'cmd'},'k', function() switcher:previous() end)
+
+function getChromeMenus()
+  local app = hs.window.filter.new{'Google Chrome'}:getWindows()[1]:application()
+  local mn =  app:findMenuItem({'File', 'Open Location…'})
+  return app:selectMenuItem({'File', 'Open Location…'})
+end
--- utils.lua
+++ utils.lua
@@ -26,6 +26,51 @@ function module.strToTable(str)
   end
   return t
 end
+--- --------------------------
+--- Keymap utils
+--- --------------------------
+local REPEAT_FASTER = 10 * 1000
+local REPEAT_SLOWER = 100 * 1000
+local NO_REPEAT = -1
+
+local function keyStroke(mod, key, repeatDelay)
+    hs.eventtap.event.newKeyEvent(mod, key, true):post()
+    if repeatDelay <= 0 then
+        repeatDelay = REPEAT_FASTER
+    end
+    hs.timer.usleep(repeatDelay)
+    hs.eventtap.event.newKeyEvent(mod, key, false):post()
+end
+
+local function keyStrokeSystem(key, repeatDelay)
+    hs.eventtap.event.newSystemKeyEvent(key, true):post()
+    if repeatDelay <= 0 then
+        repeatDelay = REPEAT_FASTER
+    end
+    hs.timer.usleep(repeatDelay)
+    hs.eventtap.event.newSystemKeyEvent(key, false):post()
+end
+
+-- Map sourceKey + sourceMod -> targetKey + targetMod
+module.keymap = function(sourceKey, sourceMod, targetKey, targetMod, repeatDelay)
+    sourceMod = sourceMod or {}
+
+    repeatDelay = repeatDelay or REPEAT_FASTER
+    noRepeat = repeatDelay <= 0
+
+    local fn = nil
+    if targetMod == nil then
+        fn = hs.fnutils.partial(keyStrokeSystem, string.upper(targetKey), repeatDelay)
+    else
+        targetMod = module.splitStr(targetMod, '+')
+        fn = hs.fnutils.partial(keyStroke, targetMod, targetKey, repeatDelay)
+    end
+    if noRepeat then
+        hs.hotkey.bind(sourceMod, sourceKey, fn, nil, nil)
+    else
+        hs.hotkey.bind(sourceMod, sourceKey, fn, nil, fn)
+    end
+end
 
 --- Filter that includes full-screen apps 
 module.globalfilter = hs.window.filter.new()
